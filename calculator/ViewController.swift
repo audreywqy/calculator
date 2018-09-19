a710d1c512f880228f05e3604f59352ed2abae35
Audreyw <q344wang@edu.uwaterloo.ca>
2018-09-19 18:56:43 -0400

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Audrey Wang on 2018-09-19.
+//  Copyright © 2018 Audrey Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
